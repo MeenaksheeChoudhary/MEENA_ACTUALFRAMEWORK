5f32ee67da415544e3c92b43d369922cee100df4
amans <amans@192.168.137.36>
2024-11-07 15:48:59 +0530

    PUSH SUCCESSFULLY

---
+++ MEENA_ACTUALFRAMEWORK/src/test/java/Meena_Contacts/Meena_ToCreateContacts.java
@@ -0,0 +1,10 @@
+package Meena_Contacts;
+
+public class Meena_ToCreateContacts {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ MEENA_ACTUALFRAMEWORK/src/test/java/Meena_Org02/ToCreateOrg02.java
@@ -0,0 +1,10 @@
+package Meena_Org02;
+
+public class ToCreateOrg02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
